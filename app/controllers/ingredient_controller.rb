c3763bc2146a5249c1827c90f3777a24f84c2fc3
Teemij Dinaran <teemijamazon@gmail.com>
2021-08-10 11:59:11 +0400

    ingredient controller

---
+++ app/controllers/ingredient_controller.rb
@@ -0,0 +1,32 @@
+class IngredientController < ApplicationController
+
+  def index
+    @ingredients = Ingredients.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save
+    redirect_to ingredients_path(@ingredient)
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def destroy
+    @ingredient = Ingredient.find(params[:id])
+    @ingredient.destroy
+    redirect_to ingredients_path
+  end
+
+  private
+
+  def review_params
+    params.require(:ingredient).permit(:name, :unit_price, :unit, :category)
+  end
+end

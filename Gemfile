b38f762632288b4c582384e8275c2d1256793ef5
Teemij Dinaran <teemijamazon@gmail.com>
2021-08-16 09:37:41 +0400

    images

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'redis', '~> 4.0'
 gem 'pg_search', '~> 2.3.0'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
-
+gem 'cloudinary', '~> 1.16.0'
 gem 'devise'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'autoprefixer-rails', '10.2.5'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 User.destroy_all
 Basket.destroy_all
 DetailBasket.destroy_all
@@ -110,7 +111,10 @@ tomato_sauce.save
 p tomato_sauce.name
 
 ##########  Chocolate cake
+
+file = URI.open('https://images.pexels.com/photos/3740196/pexels-photo-3740196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
 choco_cake = Recipe.new(name: 'Chocolate Cake', description: "Preheat your oven to 180°C (gas mark 6). In a saucepan, melt the chocolate and the butter cut into pieces over a very low heat.In a bowl, add the sugar, eggs and flour. Mix well.Add the chocolate/butter mixture. Mix well.Butter and flour your cake tin and pour in the cake batter.Bake for about 20 minutes.")
+choco_cake.photo.attach(io: file, filename: 'cake.jpeg', content_type: 'image/jpeg')
 choco_cake.save
 IngredientRecipe.new(recipe_id: choco_cake.id, ingredient_id: choco.id, ingredient_quantity: 33.5).save
 IngredientRecipe.new(recipe_id: choco_cake.id, ingredient_id: flour.id, ingredient_quantity: 8.5).save
@@ -120,7 +124,9 @@ IngredientRecipe.new(recipe_id: choco_cake.id, ingredient_id: egg.id, ingredient
 p choco_cake.name
 
 ######### Mimosa egg
+file = URI.open('https://media.foodnetwork.ca/recipetracker/dmm/M/I/Mimosa_Eggs_001.jpg')
 mimosa_egg = Recipe.new(name: 'Mimosa egg', description: "Cook the eggs for 10 minutes in boiling water, then put them in cold water to stop the cooking process. Make a mayonnaise with 1 egg yolk, 1 teaspoon of mustard, 1/4 l of oil and lemon juice (optional), or use a ready-made mayonnaise sold in shops. Add the lemon juice at the last moment.Shell the eggs, cut them lengthwise, separate the whites from the yolks.In a soup plate, crumble the yolks with a fork, mix half of the crumbled yolks with the mayonnaise and reserve the rest.Fill the half-eggs with this preparation, then sprinkle each half-egg with the rest of the crumbled yolks (which makes the mimosa!!).")
+mimosa_egg.photo.attach(io: file, filename: 'egg.jpg', content_type: 'image/jpg')
 mimosa_egg.save
 IngredientRecipe.new(recipe_id: mimosa_egg.id, ingredient_id: olive_oil.id, ingredient_quantity: 62.5).save
 IngredientRecipe.new(recipe_id: mimosa_egg.id, ingredient_id: egg.id, ingredient_quantity: 1).save
@@ -129,7 +135,9 @@ p mimosa_egg.name
 
 
 ######Lasagne veg
+file = URI.open('https://images.pexels.com/photos/2765875/pexels-photo-2765875.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
 lasagne_veg = Recipe.new(name: 'Lasagne vegetarian', description: "If you are using onions, fry them in a pan or wok until they melt.Chop the tomatoes, add them to the onions, then simmer with herbs de Provence (don't hesitate!), salt and pepper.Slice the courgettes and add them to the mixture.Add tomato sauce (or tomato purée, if you don't have one), and 1 teaspoon of caster sugar (more in winter, when the tomatoes are blander). Once the mixture is ready, pile it in a large dish: 1 layer of lasagne, 1 layer of preparation, 1 layer of béchamel sauce, 1 layer of Gruyère cheese, and so on (adding a little salt each time), putting a lot of Gruyère cheese on the last layer. Bake in a hot oven (thermostat 7-200°C) for 35 minutes.")
+lasagne_veg.photo.attach(io: file, filename: 'lasagne.jpeg', content_type: 'image/jpeg')
 lasagne_veg.save
 IngredientRecipe.new(recipe_id: lasagne_veg.id, ingredient_id: lasagne.id).save
 IngredientRecipe.new(recipe_id: lasagne_veg.id, ingredient_id: cheese.id, ingredient_quantity: 100).save
@@ -146,35 +154,45 @@ p lasagne_veg.name
 
 
 #### recette a refaire
-
-pasta_sausage = Recipe.new(name: 'Pasta with italian sausage', description: 'Removed casing from sausage links and cut into 1/2 inch slices. 
-In a large skillet, brown sausage over medium heat for about 10 minutes; remove and set aside. 
-In a large skillet, heat ground beef, olive oil, garlic and onion over medium heat until meat is nicely browned; drain. 
+file = URI.open('https://images.pexels.com/photos/5710170/pexels-photo-5710170.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+pasta_sausage = Recipe.new(name: 'Pasta with italian sausage', description: 'Removed casing from sausage links and cut into 1/2 inch slices.
+In a large skillet, brown sausage over medium heat for about 10 minutes; remove and set aside.
+In a large skillet, heat ground beef, olive oil, garlic and onion over medium heat until meat is nicely browned; drain.
 Pour in tomatoes and tomato sauce; mix in salt, ground black pepper, basil, oregano, bay leaf and cooked sausage. Simmer uncovered for 1 hour, stirring occasionally.
 Bring a large pot of lightly salted water to a boil. Add pasta and cook for 8 to 10 minutes or until al dente; drain. Mix cooked sauce with hot pasta and remove bay leaf from sauce before serving.')
+pasta_sausage.photo.attach(io: file, filename: 'pasta.jpeg', content_type: 'image/jpeg')
 pasta_sausage.save
 IngredientRecipe.new(recipe_id: pasta_sausage.id, ingredient_id: italian_sausage.id, ingredient_quantity: 500).save
 IngredientRecipe.new(recipe_id: pasta_sausage.id, ingredient_id: lean_ground_beef.id, ingredient_quantity: 500).save
 p pasta_sausage.name
 
+file = URI.open('https://images.pexels.com/photos/6937447/pexels-photo-6937447.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
 egg_fried_rice = Recipe.new(name: 'Egg Fried Rice', person_number: 2, description: "Cook the rice following pack instructions, then drain, spread it out to steam-dry and set aside.
-Heat 2 tbsp of the oil in a large wok over a high heat, then add the diced onion and fry until lightly browned. 
-Add the rice, stir and toast for about 3 mins, then move to the side of the pan. Add the remaining oil, then tip in the egg mixture. 
+Heat 2 tbsp of the oil in a large wok over a high heat, then add the diced onion and fry until lightly browned.
+Add the rice, stir and toast for about 3 mins, then move to the side of the pan. Add the remaining oil, then tip in the egg mixture.
 Leave to cook a little, then mix in with the rice – stir vigorously to coat the grains. Tip into a serving bowl and scatter over the spring onion to serve.")
+egg_fried_rice.photo.attach(io: file, filename: 'rice.jpeg', content_type: 'image/jpeg')
+
 egg_fried_rice.save
 IngredientRecipe.new(recipe_id: egg_fried_rice.id, ingredient_id: rice.id, ingredient_quantity: 250).save
 IngredientRecipe.new(recipe_id: egg_fried_rice.id, ingredient_id: egg.id, ingredient_quantity: 2).save
 IngredientRecipe.new(recipe_id: egg_fried_rice.id, ingredient_id: onion.id, ingredient_quantity: 1).save
 p egg_fried_rice.name
 
+file = URI.open('https://images.pexels.com/photos/4993114/pexels-photo-4993114.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
 tarte_aux_carottes = Recipe.new(name: "Tarte aux carottes", description: "tarte avec des carottes")
+tarte_aux_carottes.photo.attach(io: file, filename: 'tarte.jpeg', content_type: 'image/jpeg')
+
 tarte_aux_carottes.save
 
 IngredientRecipe.new(recipe_id: tarte_aux_carottes.id, ingredient_id: carotte.id, ingredient_quantity: 0.7).save
 IngredientRecipe.new(recipe_id: tarte_aux_carottes.id, ingredient_id: pate_a_tarte.id, ingredient_quantity: 400).save
 p tarte_aux_carottes.name
 
+file = URI.open('https://www.pexels.com/photo/bowl-of-vegetarian-salad-with-couscous-and-vegetables-4194378/')
 couscous = Recipe.new(name: "Couscous", description: "Semoule de blé avec des carottes")
+couscous.photo.attach(io: file, filename: 'couscous.jpeg', content_type: 'image/jpeg')
+
 couscous.save
 
 ingredient_couscous1 = IngredientRecipe.new(recipe_id: couscous.id, ingredient_id: carotte.id, ingredient_quantity: 0.7)

42b3ab8de7c6d3a181e8dadc847701be2228c376
satine fouque <satine.fouque@gmail.com>
2021-08-17 17:26:09 +0400

    image ajoutée qd panier vide

--- app/controllers/detail_baskets_controller.rb
+++ app/controllers/detail_baskets_controller.rb
@@ -27,7 +27,9 @@ class DetailBasketsController < ApplicationController
       detail_basket.basket_id = current_user.baskets.first.id
       detail_basket.save
       flash[:notice] = "#{@ingredient.name} successfully added"
-      redirect_to ingredients_path(@ingredient, anchor: "ingredient-#{@ingredient.id}")
+      if @ingredient.category
+        redirect_to ingredients_path(@ingredient, anchor: "ingredient-#{@ingredient.id}")
+      end
     end
   end
 

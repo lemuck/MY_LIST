a1096091ea17387911c5c359ff9a59c81f6d7294
Teemij Dinaran <teemijamazon@gmail.com>
2021-08-17 12:22:49 +0400

    navbar photo

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :ingredients, only: [:index, :show]
 
   authenticate :user do
-    resources :users, only: [:show, :update]
+    resources :users, only: [:show, :update, :edit]
 
     resources :recipes, only: [:new, :create, :edit]
     resources :baskets, only: [:create, :show]
